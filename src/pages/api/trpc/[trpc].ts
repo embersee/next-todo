da5c4cb733c3d7045303d32dae9bf1b5e9bae439
philippshiyanov <philipp@shiyanov.com>
2022-08-23 04:30:41 +0300

    cors for trpc

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,15 +2,18 @@ import * as trpcNext from '@trpc/server/adapters/next'
 
 import { appRouter } from '../../../server/route/app.router'
 import { createContext } from '../../../server/createContext'
+import { withCors } from '../../../utils/cors'
 
-export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext,
-  onError({ error }) {
-    if (error.code === 'INTERNAL_SERVER_ERROR') {
-      console.error('something went wrong', error)
-    } else {
-      console.error(error)
-    }
-  },
-})
+export default withCors(
+  trpcNext.createNextApiHandler({
+    router: appRouter,
+    createContext,
+    onError({ error }) {
+      if (error.code === 'INTERNAL_SERVER_ERROR') {
+        console.error('something went wrong', error)
+      } else {
+        console.error(error)
+      }
+    },
+  })
+)
